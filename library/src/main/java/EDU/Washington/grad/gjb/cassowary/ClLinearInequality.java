eff08bdd696bf3b988ca1d40753883c0f6b8f7d0
Alex Birkett <alex@agens.no>
2014-10-06 15:23:37 +0200

    Granted permision by Greg J. Badros and Alan Borning to use the Apache 2.0 licence

--- library/src/main/java/EDU/Washington/grad/gjb/cassowary/ClLinearInequality.java
+++ library/src/main/java/EDU/Washington/grad/gjb/cassowary/ClLinearInequality.java
@@ -1,14 +1,22 @@
-// $Id: ClLinearInequality.java,v 1.1 2008/08/12 22:32:43 larrymelia Exp $
-//
-// Cassowary Incremental Constraint Solver
-// Original Smalltalk Implementation by Alan Borning
-// This Java Implementation by Greg J. Badros, <gjb@cs.washington.edu>
-// http://www.cs.washington.edu/homes/gjb
-// (C) 1998, 1999 Greg J. Badros and Alan Borning
-// See ../LICENSE for legal details regarding this software
-//
-// ClLinearInequality
-//
+/*
+ * Copyright (C) 1988 1999 Greg J. Badros and Alan Borning
+ * Copyright (C) 2014 Agens AS
+ *
+ * Original Smalltalk Implementation by Alan Borning
+ * This Java Implementation by Greg J. Badros
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package EDU.Washington.grad.gjb.cassowary;
 
